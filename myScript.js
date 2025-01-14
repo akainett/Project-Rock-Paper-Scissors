42b4d66b2026ad11fb23ffda75ce1017e14a1557
akainett <danettey9@gmail.com>
2025-01-14 00:26:46 -0800

    Created  prompt for player to input choice and code to return the choice made

--- myScript.js
+++ myScript.js
@@ -2,7 +2,7 @@
 let randomNumber = Math.floor(Math.random() * 3);
         
 //LET the numbers be represented by rock paper scissors, where 0 is rock, 1 is paper and 2 is scissors// 
-function getComputerChoice(randomNumber) {
+let getComputerChoice = function(randomNumber) {
     if(randomNumber === 0) {
         return "Rock";
     }else if(randomNumber === 1) {
@@ -12,3 +12,21 @@ function getComputerChoice(randomNumber) {
         }    
 }
 console.log(getComputerChoice(randomNumber));
+
+//create a prompt to ask human for input
+//after the human chooses any of the options it should alert you choose this or that
+
+let input = prompt("What's your choice!", "");
+
+let getHumanChoice = function(input) {
+    if(input === "Rock") {
+        alert("You chose Rock");
+    }else if(input === "Paper") {
+        alert("You chose Paper");
+    }else if(input === "Scissors") {
+        alert("You chose Scissors");
+    }else{
+        alert("Oops, Wrong Option");
+    }
+}
+console.log(input);
